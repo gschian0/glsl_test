a3d1db0e6fd59d32f0f830334ac6b4c62dabadae
Gennaro Schiano <gschiano@me.com>
2020-12-17 11:31:06 -0500

    upgrizzys

--- wildrotate.frag.glsl
+++ wildrotate.frag.glsl
@@ -22,20 +22,20 @@ mat2 rotate(float angle){
 }
 
 void main(){
-    vec2 coord = 6.0*gl_FragCoord.xy / u_resolution/ 0.5;
+    vec2 coord = abs(2.0*gl_FragCoord.xy / u_resolution/ 0.5-vec2(2.));
     vec3 color = vec3(0.1765, 0.6863, 0.1294);
     vec3 color2 = vec3(0.1294, 0.6863, 0.298);
-    for(int i = 0; i < 33; i++){
-    coord = scale(vec2(abs(sin(u_time)) + 0.5)) * cos(coord + 0.5-sin(u_time)-cos(u_time));
+    for(int i = 0; i < 9; i++){
+   // coord = scale(vec2(abs(sin(u_time)) + 0.5)) * cos(coord + 0.5-sin(u_time)-cos(u_time));
     coord -= vec2(0.5);
-    coord = rotate(-u_time*PI*2.0*00.01) * coord;
+   // coord = rotate(-u_time*PI*2.0*00.01) * coord;
     coord += vec2(0.5);
     color += vec3(rectshape(coord,vec2(0.7,0.7)));
-    coord *= fract(3.0*coord);
+    coord *= abs(fract(1.0*coord));
     coord *= length(-0.5*2.0) ;
-    color += color * 1.0 - mod(sin(coord.x*coord.y-u_time),1.0);
+    color += color * 1.0 - mod(sin(coord.x*coord.y-u_time),0.5);
     color *= abs(color * 1.0-color2);
 }
     
-    gl_FragColor = vec4(color-0.5,1.0);
+    gl_FragColor = vec4(color-0.5,1.0)-0.5;
 }
